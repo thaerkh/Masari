e4583ecda3dda6948ca97a5ade9599ff2c83d2e0
Thaer Khawaja <thaer.khawaja@gmail.com>
2018-12-25 12:22:54 -0800

    First pass BP multi aggregation integration
    Reverted 0648c0227f1535a11c35a7bed90c2f4ab3487cc4
    
    Upstream dependency additions
    https://github.com/monero-project/monero/commit/aacfd6e3702810c43365658da61bb1afc7470fd5
    https://github.com/monero-project/monero/commit/1aa10c4364be4ba48df06f5ff582eef10b9288c3
    https://github.com/monero-project/monero/commit/8f4ce989c2b586667d9fed4a342576472ace5ff4
    https://github.com/monero-project/monero/commit/f5d7b9933a68e70002ac74b65a9b6e3f4ccd2d24
    https://github.com/monero-project/monero/commit/112f32f068e2e6d0b8e713ccf49090b73e542b5d
    https://github.com/monero-project/monero/commit/e9164bb38ba6105810597c8fa0d5a9bdfa295b07
    https://github.com/monero-project/monero/commit/9ff6e6a0a75d24fb3189e695def0b51b4fa67e92
    https://github.com/monero-project/monero/commit/939bc2233281c47427c9987fc5310cfb77b085f9
    https://github.com/monero-project/monero/commit/0793184bd008b51cc6865738a86bcd10792d42e0
    https://github.com/monero-project/monero/commit/f34e2e20bdfbefa171e3bbb46615b2b0513ca394
    https://github.com/monero-project/monero/commit/639ca3b1fa9c153a99f1d4e9bb488bd02d282e46
    https://github.com/monero-project/monero/commit/cb9c7972b62d027303d72a9da218f2b6106b4ba3
    https://github.com/monero-project/monero/commit/d7a6b72c15f898c45a71831cca04cf0fc968ad91
    https://github.com/monero-project/monero/commit/25c15dca5c0ea58f5033309372eb154c48c81a3c
    
    https://github.com/monero-project/monero/pull/4219 e6d36c17015e179aaa21f2353bdc608967833303
    Integration excludes per byte fee changes
    
    Co-authored-by: moneromooo-monero <moneromooo-monero@users.noreply.github.com>
    Co-authored-by: stoffu <stoffu@protonmail.ch>

--- src/ringct/rctTypes.cpp
+++ src/ringct/rctTypes.cpp
@@ -28,6 +28,7 @@
 // STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF
 // THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
+#include "misc_log_ex.h"
 #include "rctTypes.h"
 using namespace crypto;
 using namespace std;
@@ -207,6 +208,50 @@ namespace rct {
             vali = (xmr_amount)(vali * 2 + amountb[j]);
         }
         return vali;
+    }
+
+    bool is_rct_simple(int type)
+    {
+        switch (type)
+        {
+            case RCTTypeSimple:
+            case RCTTypeBulletproof:
+                return true;
+            default:
+                return false;
+        }
+    }
+
+    bool is_rct_bulletproof(int type)
+    {
+        switch (type)
+        {
+            case RCTTypeBulletproof:
+                return true;
+            default:
+                return false;
+        }
+    }
+
+    size_t n_bulletproof_amounts(const Bulletproof &proof)
+    {
+        CHECK_AND_ASSERT_MES(proof.L.size() >= 6, 0, "Invalid bulletproof L size");
+        CHECK_AND_ASSERT_MES(proof.L.size() <= 31, 0, "Insane bulletproof L size");
+        return 1 << (proof.L.size() - 6);
+    }
+
+    size_t n_bulletproof_amounts(const std::vector<Bulletproof> &proofs)
+    {
+        size_t n = 0;
+        for (const Bulletproof &proof: proofs)
+        {
+            size_t n2 = n_bulletproof_amounts(proof);
+            CHECK_AND_ASSERT_MES(n2 < std::numeric_limits<uint32_t>::max() - n, 0, "Invalid number of bulletproofs");
+            if (n2 == 0)
+                return 0;
+            n += n2;
+        }
+        return n;
     }
 
 }
